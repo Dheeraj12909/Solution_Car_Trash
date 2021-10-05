c0c5f0f19da9f0d704b09c895080078eebe19e3a
Dheeraj12909 <52652072+Dheeraj12909@users.noreply.github.com>
2021-10-05 14:35:35 +0530

    Add files via upload

---
+++ configs/meta_config.py
@@ -0,0 +1,11 @@
+primary_person_use_file = "file:///home/hadoop/dheeraj/case/Solution/Data/Primary_Person_use.csv"
+
+restrict_use_file = "file:///home/hadoop/dheeraj/case/Solution/Data/Restrict_use.csv"
+
+units_use_file = "file:///home/hadoop/dheeraj/case/Solution/Data/Units_use.csv"
+
+damages_use_file = "file:///home/hadoop/dheeraj/case/Solution/Data/Damages_use.csv"
+
+charges_use_file = "file:///home/hadoop/dheeraj/case/Solution/Data/Charges_use.csv"
+
+endorse_use_file = "file:///home/hadoop/dheeraj/case/Solution/Data/Endorse_use.csv"
